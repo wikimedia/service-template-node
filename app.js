085debe48f1a2660a83c4b378666f38c04a5e45a
Marko Obrovac <marko.obrovac@gmail.com>
2016-05-30 17:18:38 +0200

    Set up the request templates once on start-up

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var compression = require('compression');
 var bodyParser = require('body-parser');
 var fs = BBPromise.promisifyAll(require('fs'));
 var sUtil = require('./lib/util');
+var apiUtil = require('./lib/api-util');
 var packageInfo = require('./package.json');
 var yaml = require('js-yaml');
 
@@ -62,6 +63,9 @@ function initApp(options) {
     app.conf.log_header_whitelist = new RegExp('^(?:' + app.conf.log_header_whitelist.map(function(item) {
         return item.trim();
     }).join('|') + ')$', 'i');
+
+    // set up the request templates for the APIs
+    apiUtil.setupApiTemplates(app);
 
     // set up the spec
     if(!app.conf.spec) {
