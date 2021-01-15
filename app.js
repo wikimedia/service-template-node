11cca389b0f2ed907fb7dd8013dc8b8d6e5ebf26
Bharatkhatri <bharatkhatri351@gmail.com>
2021-01-15 17:12:28 +0530

    Delete unused static html file
    delete unused static file and disable serving from app.js
    bug: T261712

--- app.js
+++ app.js
@@ -233,8 +233,6 @@ module.exports = (options) => {
     return initApp(options)
     .then((app) => loadRoutes(app, `${__dirname}/routes`))
     .then((app) => {
-        // serve static files from static/
-        app.use('/static', express.static(`${__dirname}/static`));
         return app;
     }).then(createServer);
 
