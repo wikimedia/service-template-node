2f4671c7510a83056c1129d1cb8c0b1396336bf1
James D. Forrester <jforrester@wikimedia.org>
2022-04-07 20:28:45 +0100

    eslint: Follow coding standard for indenting

--- routes/v1.js
+++ routes/v1.js
@@ -32,38 +32,38 @@ const router = sUtil.router();
  */
 router.get('/siteinfo/:prop?', (req, res) => {
 
-    // construct the query for the MW Action API
-    const apiQuery = {
-        action: 'query',
-        meta: 'siteinfo'
-    };
+	// construct the query for the MW Action API
+	const apiQuery = {
+		action: 'query',
+		meta: 'siteinfo'
+	};
 
-    // send it
-    return apiUtil.mwApiGet(req, apiQuery)
-    // and then return the result to the caller
-    .then((apiRes) => {
-        // do we have to return only one prop?
-        if (req.params.prop) {
-            // check it exists in the response body
-            if (apiRes.body.query.general[req.params.prop] === undefined) {
-                // nope, error out
-                throw new HTTPError({
-                    status: 404,
-                    type: 'not_found',
-                    title: 'No such property',
-                    detail: `Property ${req.params.prop} not found in MW API response!`
-                });
-            }
-            // ok, return that prop
-            const ret = {};
-            ret[req.params.prop] = apiRes.body.query.general[req.params.prop];
-            res.status(200).json(ret);
-            return;
-        }
-        // set the response code as returned by the MW API
-        // and return the whole response (contained in body.query.general)
-        res.status(200).json(apiRes.body.query.general);
-    });
+	// send it
+	return apiUtil.mwApiGet(req, apiQuery)
+	// and then return the result to the caller
+		.then((apiRes) => {
+			// do we have to return only one prop?
+			if (req.params.prop) {
+				// check it exists in the response body
+				if (apiRes.body.query.general[req.params.prop] === undefined) {
+					// nope, error out
+					throw new HTTPError({
+						status: 404,
+						type: 'not_found',
+						title: 'No such property',
+						detail: `Property ${req.params.prop} not found in MW API response!`
+					});
+				}
+				// ok, return that prop
+				const ret = {};
+				ret[req.params.prop] = apiRes.body.query.general[req.params.prop];
+				res.status(200).json(ret);
+				return;
+			}
+			// set the response code as returned by the MW API
+			// and return the whole response (contained in body.query.general)
+			res.status(200).json(apiRes.body.query.general);
+		});
 
 });
 
@@ -80,12 +80,12 @@ router.get('/siteinfo/:prop?', (req, res) => {
  */
 function getBody(req) {
 
-    // get the page
-    return apiUtil.restApiGet(req, `page/html/${req.params.title}`)
-    .then((callRes) => {
-        // and then load and parse the page
-        return BBPromise.resolve(domino.createDocument(callRes.body));
-    });
+	// get the page
+	return apiUtil.restApiGet(req, `page/html/${req.params.title}`)
+		.then((callRes) => {
+			// and then load and parse the page
+			return BBPromise.resolve(domino.createDocument(callRes.body));
+		});
 
 }
 
@@ -95,12 +95,12 @@ function getBody(req) {
  */
 router.get('/page/:title', (req, res) => {
 
-    // get the page's HTML directly
-    return getBody(req)
-    // and then return it
-    .then((doc) => {
-        res.status(200).type('html').end(doc.body.innerHTML);
-    });
+	// get the page's HTML directly
+	return getBody(req)
+	// and then return it
+		.then((doc) => {
+			res.status(200).type('html').end(doc.body.innerHTML);
+		});
 
 });
 
@@ -110,34 +110,34 @@ router.get('/page/:title', (req, res) => {
  */
 router.get('/page/:title/lead', (req, res) => {
 
-    // get the page's HTML directly
-    return getBody(req)
-    // and then find the leading section and return it
-    .then((doc) => {
-        let leadSec = '';
-        // find all paragraphs directly under the content div
-        const ps = doc.querySelectorAll('p') || [];
-        for (let idx = 0; idx < ps.length; idx++) {
-            const child = ps[idx];
-            // find the first paragraph that is not empty
-            if (!/^\s*$/.test(child.innerHTML)) {
-                // that must be our leading section
-                // so enclose it in a <div>
-                leadSec = `<div id="lead_section">${child.innerHTML}</div>`;
-                break;
-            }
-        }
-        res.status(200).type('html').end(leadSec);
-    });
+	// get the page's HTML directly
+	return getBody(req)
+	// and then find the leading section and return it
+		.then((doc) => {
+			let leadSec = '';
+			// find all paragraphs directly under the content div
+			const ps = doc.querySelectorAll('p') || [];
+			for (let idx = 0; idx < ps.length; idx++) {
+				const child = ps[idx];
+				// find the first paragraph that is not empty
+				if (!/^\s*$/.test(child.innerHTML)) {
+					// that must be our leading section
+					// so enclose it in a <div>
+					leadSec = `<div id="lead_section">${child.innerHTML}</div>`;
+					break;
+				}
+			}
+			res.status(200).type('html').end(leadSec);
+		});
 
 });
 
 module.exports = (appObj) => {
 
-    return {
-        path: '/',
-        api_version: 1,
-        router
-    };
+	return {
+		path: '/',
+		api_version: 1,
+		router
+	};
 
 };
